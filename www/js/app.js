73ee108db26fe5bdbdf1de4b35120f3e7667803d
Marlon Ruiz <marlon_ru@hotmail.com>
2016-02-08 22:00:59 -0600

    aw

--- www/js/app.js
+++ www/js/app.js
@@ -5,6 +5,32 @@ document.addEventListener('deviceready',function() {
       AUTH0_DOMAIN
     );
 
+    var userProfile;
+
+    $('.btn-login').click(function(e) {
+      e.preventDefault();
+      lock.show(function(err, profile, token) {
+        if (err) {
+          // Error callback
+          console.log("There was an error");
+          alert("There was an error logging in");
+        } else {
+          // Success calback
+
+          // Save the JWT token.
+          localStorage.setItem('userToken', token);
+
+          // Save the profile
+          userProfile = profile;
+
+          $('.login-box').hide();
+          $('.logged-in-box').show();
+          $('.nickname').text(profile.nickname);
+          $('.nickname').text(profile.name);
+          $('.avatar').attr('src', profile.picture);
+        }
+      });
+    });
 
     $.ajaxSetup({
       'beforeSend': function(xhr) {
